da3e895669d8b6bd3963070436a655728190a2ac
Maxime Escourrou <maxime@escourrou.ovh>
2019-02-18 18:25:27 +0100

    Initial commit

---
+++ src/core/Widget.cpp
@@ -0,0 +1,12 @@
+#include "Widget.hpp"
+
+namespace gameGUI {
+namespace core {
+
+Widget::Widget() : sf::Shape()
+{
+
+}
+
+} // namespace core
+} // namespace gameGUI
---
+++ src/core/Widget.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <SFML/Graphics/Shape.hpp>
+
+namespace gameGUI {
+namespace core {
+
+class Widget : public ::sf::Shape
+{
+public:
+    Widget();
+};
+
+} // namespace core
+} // namespace gameGUI
+
